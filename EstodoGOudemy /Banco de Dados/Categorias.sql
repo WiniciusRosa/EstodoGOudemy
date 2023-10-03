6568705a8b0a003f031348e1cb6dc652e20877d4
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-10-03 11:22:43 -0400

    Update Categorias.sql

--- EstodoGOudemy /Banco de Dados/Categorias.sql
+++ EstodoGOudemy /Banco de Dados/Categorias.sql
@@ -18,7 +18,7 @@ select * from categorias where id = $1 limit 1;
 select * from categorias where user_id = $1 and type = 2 and title like $3 and description like $4;
 
 -- name: UpdateCategorias :one
-Update categorias Set title = $2, description = $3 Where id = $1;
+Update categorias Set title = $2, description = $3 Where id = $1 returning *;
 
 -- name: DeleteCategorias :exec
 Delete from categorias where id = $1;
